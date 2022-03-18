c804862c2f72c9aad05944a6c8cb6a4540f1e865
Thomas Cardwell <79612290+tdgcardwell@users.noreply.github.com>
2022-03-18 14:15:21 -0400

    example scripts

---
+++ example-edit.js
@@ -0,0 +1,130 @@
+/*
+
+So, theoretically, the whole list could be a ul, with the cards each being a li.
+
+<ul id="invitedList">
+        <li class="responded">
+          <span>Tuan</span>
+          <label>Confirmed<input type="checkbox" checked="true"></label>
+          <button>edit</button><button>remove</button>
+        </li>
+
+*/
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('registrar');
+  const input = form.querySelector('input');
+
+  const mainDiv = document.querySelector('.main');
+  const ul = document.getElementById('invitedList');
+
+
+// filter (show and hide)
+  const div = document.createElement('div');
+  const filterLabel = document.createElement('label');
+  const filterCheckBox = document.createElement('input');
+
+  filterLabel.textContent = "Hide those who haven't responded";
+  filterCheckBox.type = 'checkbox';
+  div.appendChild(filterLabel);
+  div.appendChild(filterCheckBox);
+  mainDiv.insertBefore(div, ul);
+  filterCheckBox.addEventListener('change', (e) => {
+    const isChecked = e.target.checked;
+    const lis = ul.children;
+    if(isChecked) {
+      for (let i = 0; i < lis.length; i += 1) {
+        let li = lis[i];
+        if (li.className === 'responded') {
+          li.style.display = '';
+        } else {
+          li.style.display = 'none';
+        }
+      }
+    } else {
+      for (let i = 0; i < lis.length; i += 1) {
+        let li = lis[i];
+        li.style.display = '';
+      }
+    }
+  });
+
+// create one
+  function createLI(text) {
+    function createElement(elementName, property, value) {
+      const element = document.createElement(elementName);
+      element[property] = value;
+      return element;
+    }
+
+    function appendToLI(elementName, property, value) {
+      const element = createElement(elementName, property, value);
+      li.appendChild(element);
+      return element;
+    }
+
+    const li = document.createElement('li');
+    appendToLI('span', 'textContent', text);
+    appendToLI('label', 'textContent', 'Confirmed')
+      .appendChild(createElement('input', 'type', 'checkbox'));
+    appendToLI('button', 'textContent', 'edit');
+    appendToLI('button', 'textContent', 'remove');
+    return li;
+  }
+
+//call create one with the relevant text
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const text = input.value;
+    input.value = '';
+    const li = createLI(text);
+    ul.appendChild(li);
+  });
+
+// mark
+  ul.addEventListener('change', (e) => {
+    const checkbox = e.target;
+    const listItem = checkbox.parentNode.parentNode;
+    const checked = checkbox.checked;
+
+    if (checked) {
+      listItem.className = 'responded';
+    } else {
+      listItem.className = '';
+    }
+  });
+
+// edit/save/remove
+  ul.addEventListener('click', (e) => {
+    if (e.target.tagName === 'BUTTON') {
+      const button = e.target;
+      const li = button.parentNode;
+      const ul = li.parentNode;
+      const action = button.textContent;
+      const nameActions = {
+        remove: () => {
+          ul.removeChild(li);
+        },
+        edit: () => {
+          const span = li.firstElementChild;
+          const input = document.createElement('input');
+          input.type = 'text';
+          input.value = span.textContent;
+          li.insertBefore(input, span);
+          li.removeChild(span);
+          button.textContent = 'save';
+        },
+        save: () => {
+          const input = li.firstElementChild;
+          const span = document.createElement('span');
+          span.textContent = input.value;
+          li.insertBefore(span, input);
+          li.removeChild(input);
+          button.textContent = 'edit';
+        }
+      };
+
+      // select and run action in button's name
+      nameActions[action]();
+    }
+  });
+});
